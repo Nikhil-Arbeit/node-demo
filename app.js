8197f2c5801bbbb0beca94d5d99a70aced3d198e
Nikhil <nikhil.kumar.arbeit@gmail.com>
2021-12-01 16:50:45 +0100

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000;
-app.get('/',(req,res)=>{
-    res.send('Hello World!!!!!');
-});
-app.listen(port,()=>console.log('Server is running on port'+port));
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req,res)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1>Hello World</h1>');
+})
+server.listen(port,()=>console.log('Server is running on port'+port));
